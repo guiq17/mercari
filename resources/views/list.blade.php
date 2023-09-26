aeb163028436a00136e0b7b28e2174008f7b7d7d
Rio Kamesaki <guiqi97@gmail.com>
2023-09-26 12:22:20 +0900

    プロジェクト作成

---
+++ resources/views/list.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset(mercari.css) }}">
+</head>
+<body>
+    {{-- navbar --}}
+    <nav class="navbar navbar-inverse">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapsed" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a href="" class="navbar-brand">Rakus Items</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+            {{-- 認証機能 --}}
+        </div>
+    </nav>
+    <div id="main" class="container-fluid">
+        {{-- addItemlink --}}
+    </div>
+</body>
+</html>
