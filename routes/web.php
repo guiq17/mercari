970d17e6a80f6c4fc86a4999340a3ec4bab9dbad
guiqi97@gmail.com <guiqi97@gmail.com>
2023-12-22 09:49:26 +0900

    laravel breezeインストール

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ItemsController;
-use App\Http\Controllers\ItemEditController;
-use App\Http\Controllers\ItemAddController;
-use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,18 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-Route::get('/list', [ItemsController::class, 'list'])->name('item.list');
-Route::get('/getSecondCategories', [CategoryController::class, 'getSecondCategories']);
-Route::get('/getThirdCategories', [CategoryController::class, 'getThirdCategories']);
-Route::get('/detail/{id}', [ItemsController::class, 'detail'])->name('item.detail');
-Route::get('/edit/{id}', [ItemEditController::class, 'edit'])->name('item.edit');
-Route::post('update/{id}', [ItemEditController::class, 'update'])->name('item.update');
-Route::delete('/items/{id}', [ItemEditController::class, 'destroy'])->name('item.delete');
-
-Route::get('/add', [ItemAddController::class, 'add'])->name('item.add');
-Route::post('/create', [ItemAddController::class, 'create'])->name('item.create');
-
-Route::get('/phpinfo', function () {
-    phpinfo();
-});
+require __DIR__.'/auth.php';
